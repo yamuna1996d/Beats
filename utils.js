22dd0a82a07076e69388ce5c760bd362e2d1483b
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-03-17 05:06:17 +0530

    beats

---
+++ utils.js
@@ -0,0 +1,39 @@
+
+class Beat {
+    constructor(audioSrc){
+        this.audio = new Audio(audioSrc);
+    }
+
+    play = () => {
+        this.audio.currentTime = 0;
+        this.audio.play();
+    }
+}
+
+class Button {
+    constructor(color, keyCode){
+        this.color = color;
+        this.element = document.getElementById(keyCode);
+        this.setButtonColorInHTML();
+        this.createTransitionEndListener();
+    }
+
+    setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color;
+    }
+
+    createTransitionEndListener = () => {
+        this.element.addEventListener('transitionend', ()=>{
+            this.deselect();
+        })
+    }
+    select = () => {
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+    }
+    deselect = () => {
+        this.element.style.backgroundColor = "transparent";
+        this.element.style.boxShadow = `none`;
+    }
+
+}
